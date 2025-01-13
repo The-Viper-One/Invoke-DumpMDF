ec616c91761ef4df6cd457bd7e5f9c6062853df3
The-Viper-One <68926315+The-Viper-One@users.noreply.github.com>
2025-01-13 16:29:12 +0000

    Update Invoke-DumpMDF.ps1

--- Invoke-DumpMDF.ps1
+++ Invoke-DumpMDF.ps1
@@ -1,5 +1,7 @@
 Function Invoke-DumpMDF {
-    param ($NoAdmin)
+    param (
+        [bool]$NoAdmin = $false
+    )
 
 <#
 .SYNOPSIS
@@ -16,10 +18,10 @@ Must be run as SYSTEM or administrator. Requires Diskshadow to be installed (Def
 Invoke-DumpMDF
 #>
 
-    if (-not ($NoAdmin)){
-    $Principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    if (-not ([Security.Principal.WindowsIdentity]::GetCurrent().Name -eq "NT AUTHORITY\SYSTEM" -or $Principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
-    return "[-] Ensure this script is run with administrative privileges."
+    if (-not $NoAdmin) {
+        $Principal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        if (-not ([Security.Principal.WindowsIdentity]::GetCurrent().Name -eq "NT AUTHORITY\SYSTEM" -or $Principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+            throw "[-] Ensure this script is run with administrative privileges."
         }
     }
 
